aec1fdbb0f8a7d9b34710ee3f246bb3d8bf0386c
Denis Mazur <kilerden2002@gmail.com>
2026-02-20 16:38:26 +1000

    Update

---
+++ database.py
@@ -0,0 +1,115 @@
+from datetime import datetime
+from sqlalchemy import DATETIME, VARCHAR, Column, BINARY, INT, ForeignKey, TEXT, Index, delete, desc, event, update, func
+from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy import create_engine, select, insert
+import secrets
+import crypt
+import json
+import config
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(INT, primary_key=True, autoincrement=True)
+    login = Column(VARCHAR(25), nullable=False)
+    encrypted_private_key = Column(BINARY(48), nullable=False)
+    public_key = Column(BINARY(32), nullable=False)
+
+
+class Service(Base):
+    __tablename__ = 'services'
+
+    # id = Column(INT, primary_key=True, autoincrement=True)
+    id = Column(VARCHAR(25), primary_key=True, nullable=False)
+
+
+class BucketItem(Base):
+    __tablename__ = 'bucket_items'
+
+    id = Column(INT, primary_key=True, autoincrement=True)
+    user_id = Column(ForeignKey(User.id), nullable=False)
+    document_id = Column(TEXT, nullable=False)
+    service_id = Column(ForeignKey(Service.id), nullable=False)
+
+
+# class Log(Base):
+#     __tablename__ = 'logs'
+
+#     id = Column(INT, primary_key=True, autoincrement=True)
+#     date_time = Column(DATETIME, nullable=False)
+#     action = Column(VARCHAR(255), nullable=False)
+#     message = Column(TEXT, nullable=True)
+#     result = Column(INT, nullable=False)
+#     user_id = Column(ForeignKey(User.id), nullable=True)
+#     group_id = Column(ForeignKey(Group.id), nullable=True)
+#     collection_id = Column(ForeignKey(Collection.id), nullable=True)
+
+
+@event.listens_for(Service.__table__, 'after_create')
+def insert_initial_user_roles(target, connection, **kw):
+    connection.execute(target.insert(), [
+        {'id': 'storage-api'}
+    ])
+
+
+class MainDatabase:
+    def __init__(self):
+        self.engine = create_engine(
+            f'mariadb+pymysql://{config.db_user}:{config.db_password}@localhost/main?charset=utf8mb4',
+            pool_pre_ping=True,
+            pool_recycle=3600,
+            pool_size=10,
+            max_overflow=20,
+            echo=False,
+        )
+        self.connection = self.engine.connect()
+        Base.metadata.create_all(self.engine)
+
+    def get_user_id(self, login: str) -> int | None:
+        with Session(self.engine) as session:
+            query = select(User.id).where(User.login == login)
+            user_id = session.execute(query).scalar()
+            return user_id
+
+    def get_username(self, user_id: int) -> str | None:
+        with Session(self.engine) as session:
+            query = select(User.login).where(User.id == user_id)
+            username = session.execute(query).scalar()
+            return username
+
+    def add_to_bucket(self, user_id: int, document_id: str, service_id: str) -> int:
+        with Session(self.engine) as session:
+            query = insert(BucketItem).values(
+                user_id=user_id, document_id=document_id, service_id=service_id).returning(BucketItem.id)
+            item_id = session.execute(query).scalar_one()
+            session.commit()
+            return item_id
+
+    def get_bucket(self, user_id: int) -> list:
+        with Session(self.engine) as session:
+            query = select(BucketItem.id, BucketItem.document_id,
+                           BucketItem.service_id).where(BucketItem.user_id == user_id)
+            bucket_items = session.execute(query).all()
+
+            result = []
+            for item in bucket_items:
+                result.append(
+                    {'id': item[0], 'document_id': item[1], 'service_id': item[2]})
+            return result
+
+    def delete_from_bucket(self, user_id: int, item_id: int):
+        with Session(self.engine) as session:
+            query = delete(BucketItem).where((BucketItem.user_id == user_id) & (BucketItem.id == item_id))
+            session.execute(query)
+            session.commit()
+
+    def clear_bucket(self, user_id: int):
+        with Session(self.engine) as session:
+            query = delete(BucketItem).where(BucketItem.user_id == user_id)
+            session.execute(query)
+            session.commit()
--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from typing import Annotated
 from build_query import build_queries_for_categories
 import config
 import httpx
+from database import MainDatabase
 from sessions import WebSessionsBase
-
 from opensearch import OpenSearchManager
 
 web_sessions = WebSessionsBase()
 opensearch = OpenSearchManager()
+database = MainDatabase()
 
 
 @asynccontextmanager
@@ -41,6 +42,20 @@ class SearchRequest(BaseModel):
     token: str
     filters: dict
     categories: list[str]
+
+
+class AddToBucketRequest(BaseModel):
+    document_id: str
+    token: str
+    service_id: str
+
+
+class DeleteFromBucketRequest(BaseModel):
+    token: str
+    item_id: int
+
+class ClearBucketRequest(BaseModel):
+    token: str
 
 
 @app.get('/auth')  # safe+ logs+
@@ -80,28 +95,83 @@ async def check(token: str) -> dict[str, str | bool]:
     if user:
         username, org = user
         return {'authenticated': True, 'username': username, 'org': org}
-    raise HTTPException(status_code=401, detail='Token not found')
+    raise HTTPException(status_code=401, detail='Token invalid')
 
 
 @app.get('/categories')
 async def get_categories(token: str) -> dict:
     user = await web_sessions.get_username_and_org(token)
     if user:
-        categories = json.loads(open('categories.json', 'r').read())['categories']
+        categories = json.loads(open('categories.json', 'r').read())[
+            'categories']
         return categories
-    raise HTTPException(status_code=401, detail='Token not found')
+    raise HTTPException(status_code=401, detail='Token invalid')
 
 
 @app.post('/search')
 async def search(request: SearchRequest) -> dict:
     session = await web_sessions.get_session(request.token[:32])
     if session:
-        queries = build_queries_for_categories(categories=request.categories, text=request.text, filters=request.filters)
+        queries = build_queries_for_categories(
+            categories=request.categories, text=request.text, filters=request.filters)
         documents = await opensearch.search_documents(queries=queries, jwt_token=session['jwt_token'])
-        print(queries)
-        # for document in documents:
-        #     result.append(document)
         return documents
+    else:
+        raise HTTPException(
+            status_code=401,
+            detail='Token invalid'
+        )
+
+
+@app.post('/add_to_bucket')
+async def add_to_bucket(request: AddToBucketRequest) -> int | None:
+    session = await web_sessions.get_session(request.token[:32])
+    if session:
+        user_id = database.get_user_id(session['username'])
+        if user_id:
+            return database.add_to_bucket(user_id=user_id, document_id=request.document_id, service_id=request.service_id)
+    else:
+        raise HTTPException(
+            status_code=401,
+            detail='Token invalid'
+        )
+
+
+@app.get('/get_bucket')
+async def get_bucket(token) -> list | None:
+    session = await web_sessions.get_session(token[:32])
+    if session:
+        user_id = database.get_user_id(session['username'])
+        if user_id:
+            return database.get_bucket(user_id=user_id)
+    else:
+        raise HTTPException(
+            status_code=401,
+            detail='Token invalid'
+        )
+
+
+@app.post('/delete_from_bucket')
+async def delete_from_bucket(request: DeleteFromBucketRequest) -> list | None:
+    session = await web_sessions.get_session(request.token[:32])
+    if session:
+        user_id = database.get_user_id(session['username'])
+        if user_id:
+            return database.delete_from_bucket(user_id=user_id, item_id=request.item_id)
+    else:
+        raise HTTPException(
+            status_code=401,
+            detail='Token invalid'
+        )
+
+
+@app.post('/clear_bucket')
+async def clear_bucket(request: ClearBucketRequest):
+    session = await web_sessions.get_session(request.token[:32])
+    if session:
+        user_id = database.get_user_id(session['username'])
+        if user_id:
+           database.clear_bucket(user_id=user_id)
     else:
         raise HTTPException(
             status_code=401,
